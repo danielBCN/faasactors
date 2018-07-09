d4a66ec4bf1885a30e45a24e66a4c6e0fbde94e9
Daniel Barcelona <daniel.gandesa@gmail.com>
2018-07-09 10:52:18 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='faasactors',
+    version='0.0.1',
+    packages=['tests', 'faasactors'],
+    url='',
+    license='',
+    author='Daniel',
+    author_email='daniel.barcelona@urv.cat',
+    description='Actor model over FaaS'
+)
