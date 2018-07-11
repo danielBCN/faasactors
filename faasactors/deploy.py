ec6c2a007607668de01221532422048bf0252970
davizzard <davizzard@gmail.com>
2018-07-11 06:34:37 -0400

    Deploy code and dependencies

---
+++ faasactors/deploy.py
@@ -0,0 +1,73 @@
+"""
+Utils to create lambda functions.
+"""
+import uuid
+import boto3
+
+
+from faasactors.utils.packaging import package_with_dependencies
+from faasactors.config import (AWS_ROLE_ARN, VPC_CONFIG, REDIS_HOST, REDIS_PORT, TOPIC_ARN,
+                    AWS_REGION, RESULT_QUEUE, SHUFFLER)
+
+
+lambdacli = boto3.client('lambda', region_name=AWS_REGION)
+sqscli = boto3.resource('sqs', region_name=AWS_REGION)
+
+
+def uuid_str():
+    """ Generate an uninque id."""
+    return str(uuid.uuid4())
+
+
+def get_lambda_client():
+    """ Just returns a boto3 lambda client connection."""
+    return lambdacli
+
+
+def new_lambda(name, handler):
+    """
+    Packages all files that the function *handler* depends on into a zip.
+    Creates a new lambda with name *name* on AWS using that zip.
+    This one does not have VPC config. So it has acces to extern services
+    such as SNS and SQS. (But can't connect directly to Redis)
+    """
+    # zip function-module and dependencies
+    zipfile, lamhand = package_with_dependencies(handler)
+
+    # create the new lambda by uploading the zip.
+    response = lambdacli.create_function(
+        FunctionName=name,
+        Runtime='python3.6',
+        Role=AWS_ROLE_ARN,
+        Handler=lamhand,
+        Code={'ZipFile': zipfile.getvalue()},
+        Publish=True,
+        Description='Lambda with cloud object.',
+        Timeout=60,
+        MemorySize=128
+        # VpcConfig=VPC_CONFIG,
+        # DeadLetterConfig={
+        #     'TargetArn': 'string'
+        # },
+        #  Environment={
+        #    'Variables': {  # FIXME: Variables should be requested. Timeout too.
+        #        'RESULT_QUEUE': RESULT_QUEUE
+        #    }
+        #  }
+        # KMSKeyArn='string',
+        # TracingConfig={
+        #     'Mode': 'Active'|'PassThrough'
+        # },
+        # Tags={
+        #     'string': 'string'
+        # }
+    )
+    print(response)
+    print("New lambda {} created successfully.".format(name))
+
+
+def delete_lambda(name):
+    """ Deletes a lambda function from AWS with name *name*."""
+    response = lambdacli.delete_function(FunctionName=name)
+    print(response)
+    print("Lambda {} deleted successfully.".format(name))
---
+++ faasactors/utils/packaging.py
@@ -0,0 +1,44 @@
+import sys
+import os
+import io
+import zipfile
+
+
+from faasactors.utils.module_dependency import ModuleDependencyAnalyzer
+import faasactors.utils.default_preinstalls
+
+
+def package_with_dependencies(func):
+    """
+    Packages the *func* module and all dependence modules on a zip.
+    *func* cannot be defined at the __main__ module.
+    Returns a tuple with the zip file (in memory),
+    and the path to that function inside the package.
+    """
+    print(func.__module__)
+    function_name = func.__name__
+    module_name = func.__module__
+    module = sys.modules[module_name]
+
+    # Get dependencies
+    modulean = ModuleDependencyAnalyzer()
+    preinstalled_modules = [name for name, _ in faasactors.utils.default_preinstalls.modules]
+    modulean.ignore(preinstalled_modules)
+    modulean.add(module_name)
+
+    mod_paths = modulean.get_and_clear_paths()
+
+    # Package it all in a zip
+    file_like_object = io.BytesIO()
+    with zipfile.ZipFile(file_like_object, 'w') as newzip:
+        for mod in mod_paths:
+            if os.path.isdir(mod):
+                 for root, dirs, files in os.walk(mod):
+                     for file_ in files:
+                         newzip.write(os.path.join(root, file_),
+                                      os.path.relpath(os.path.join(root, file_),
+                                                      os.path.join(mod, '..')))
+            else:
+                newzip.write(mod, os.path.basename(mod))
+
+    return file_like_object, module_name + '.' + function_name
